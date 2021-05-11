52a8801b03ec79ed577d462a77521d253188211c
torru00 <84038076+torru00@users.noreply.github.com>
2021-05-12 02:43:52 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `안녕 ${text}`;
+}
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+function init() {
+  loadName();
+}
+init();
